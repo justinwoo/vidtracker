2a695d1f7cb459f5e12284e267a41af1bbf093c3
justinwoo <moomoowoo@gmail.com>
2017-03-16 03:27:07 +0200

    return files in reverse date order

--- src/Main.purs
+++ src/Main.purs
@@ -12,15 +12,17 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Except (runExcept)
+import Data.Array (reverse, sortBy)
 import Data.Either (Either(..))
 import Data.Foreign.Class (class IsForeign, readJSON, write)
 import Data.Generic.Rep (class Generic)
 import Data.HTTP.Method (Method(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (unwrap)
+import Data.Traversable (traverse)
 import Data.Tuple (Tuple(Tuple))
 import Global.Unsafe (unsafeStringify)
-import Hyper.Middleware (Middleware, lift')
+import Hyper.Middleware (lift')
 import Hyper.Middleware.Class (getConn)
 import Hyper.Node.FileServer (fileServer)
 import Hyper.Node.Server (defaultOptions, runServer)
@@ -28,13 +30,22 @@ import Hyper.Request (readBody)
 import Hyper.Response (headers, respond, writeStatus)
 import Hyper.Status (statusBadRequest, statusNotFound, statusOK)
 import Node.Buffer (BUFFER)
+import Node.ChildProcess (CHILD_PROCESS, defaultSpawnOptions, spawn)
 import Node.Encoding (Encoding(UTF8))
 import Node.FS (FS)
-import Node.FS.Aff (readdir)
+import Node.FS.Aff (readdir, stat)
+import Node.FS.Stats (Stats(..), modifiedTime)
 import Node.HTTP (HTTP)
 import Node.Path (concat)
 import Node.Process (PROCESS, lookupEnv)
 import SQLite3 (DBEffects, newDB, queryDB)
+
+newtype OpenRequest = OpenRequest
+  { path :: String
+  }
+derive instance grOR :: Generic OpenRequest _
+instance ifOR :: IsForeign OpenRequest where
+  read = DFG.readGeneric $ DFG.defaultOptions {unwrapSingleConstructors = true}
 
 newtype UpdateRequest = UpdateRequest
   { path :: String
@@ -49,6 +60,7 @@ type AppEffects eff =
   , console :: CONSOLE
   , db :: DBEffects
   , process :: PROCESS
+  , cp :: CHILD_PROCESS
   , http :: HTTP
   , fs :: FS
   , buffer :: BUFFER
@@ -77,7 +89,18 @@ main = launchAff do
       writeStatus statusOK
       :*> headers [Tuple "Content-Type" "application/json"]
       :*> respond json
-    readFiles path = lift' $ unsafeStringify <<< write <$> readdir path
+    readFiles path = lift' $ unsafeStringify <<< write <$> readdir'
+      where
+        readdir' = do
+          withStats <- traverse pairWithStat =<< readdir path
+          pure $ extractStat <$> sortByDate withStats
+        pairWithStat file = do
+          s <- stat $ concat [path, file]
+          pure (Tuple file s)
+        sortByDate = reverse <<< sortBy compareDates
+        compareDates (Tuple _ a) (Tuple _ b) =
+          compare (modifiedTime a) (modifiedTime b)
+        extractStat (Tuple file _) = file
     handleConn dir db conn =
       case Tuple conn.request.method conn.request.url of
         Tuple (Left GET) "/api/files" -> do
@@ -87,10 +110,22 @@ main = launchAff do
           watched
         Tuple (Left POST) "/api/update" -> do
           update
+        Tuple (Left POST) "/api/open" -> do
+          open
         _ -> fileServer "dist" notFound
         where
           bind = ibind
           queryDB' q p = lift' $ queryDB db q p
+          open = do
+            body <- readBody
+            case runExcept $ readJSON body of
+              Right (OpenRequest or) -> do
+                _ <- liftEff $ spawn "explorer" (pure $ concat [dir, or.path]) defaultSpawnOptions
+                respondJSON "{}"
+              Left e -> do
+                writeStatus statusBadRequest
+                headers []
+                respond $ "you gave me bad JSON!!!\n" <> show e <> "\nin\n" <> body
           update = do
             body <- readBody
             case runExcept $ readJSON body of
