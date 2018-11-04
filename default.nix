f78b3df57eaf5b122f0a0b51cc4e3c246bf96f88
justinwoo <moomoowoo@gmail.com>
2018-11-05 01:05:40 +0200

    now i have psc-package2nix in easy-purescript-nix

--- default.nix
+++ default.nix
@@ -4,22 +4,14 @@ let
   easy-ps = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "43b18de";
-    sha256 = "0c36pxafmlyq643kvyq61rw8z3h5dbg5gjb6mi7rxb5bsasqwxp8";
+    rev = "8899121af7ad2a92340d67ef7c0cf2cf03297a2a";
+    sha256 = "0pi5l9ycmfnqyzkwh6l4b5gsas0kl5jvkgb5b4ip8x8plk6aclmp";
   });
-
-  psc-package2nix = import (pkgs.fetchFromGitHub {
-    owner = "justinwoo";
-    repo = "psc-package2nix";
-    rev = "409aab26afa0784eb90440da33b1ad4d56aedb93";
-    sha256 = "11j7ny8qsv8361h88wrmmmzbhg4m5g9bbg5v3im3mfs0nhhkbrz9";
-  }) {};
 
 in pkgs.stdenv.mkDerivation {
   name = "easy-purescript";
 
   buildInputs = easy-ps.buildInputs ++ [
-    psc-package2nix
     pkgs.jq
     pkgs.nix-prefetch-git
   ];
