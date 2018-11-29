63123a2cb99f5aacb553f40fa6b486b59f2a3b20
e_ntyo <entyo.contact@gmail.com>
2018-11-29 18:18:18 +0900

    Update default.nix

--- default.nix
+++ default.nix
@@ -4,8 +4,8 @@ let
   easy-ps = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "8899121af7ad2a92340d67ef7c0cf2cf03297a2a";
-    sha256 = "0pi5l9ycmfnqyzkwh6l4b5gsas0kl5jvkgb5b4ip8x8plk6aclmp";
+    rev = "dac3520da91bf1b2d152d468700b75be5599b784";
+    sha256 = "02lcmsscbq1k3c8ap03xxbrf4vbwi1al6hsvfsr3sry7xj8f7ca4";
   });
 
 in pkgs.stdenv.mkDerivation {
