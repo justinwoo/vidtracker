679d8f5ba401c0fd90bce1c9061cd1645d5b4f33
justinwoo <moomoowoo@gmail.com>
2019-03-10 00:01:26 +0200

    cleanup unused files

--- default.nix
+++ default.nix
@@ -1,7 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  easy-ps = import ./easy-ps.nix { inherit pkgs; };
+  easy-ps = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "5d8808480436f178ccdc6593744cb6ca642cbb6c";
+    sha256 = "0r80d4dmagvkgm44rpjszl84xwgcwdbks2x9inad7akcpmkc8nnh";
+  });
 
 in pkgs.stdenv.mkDerivation {
   name = "easy-purescript";
--- easy-ps.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {}}:
-
-import (pkgs.fetchFromGitHub {
-  owner = "justinwoo";
-  repo = "easy-purescript-nix";
-  rev = "5d8808480436f178ccdc6593744cb6ca642cbb6c";
-  sha256 = "0r80d4dmagvkgm44rpjszl84xwgcwdbks2x9inad7akcpmkc8nnh";
-})
